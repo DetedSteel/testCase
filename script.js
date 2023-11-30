f78a7e4ce246392fd2f33ebf8b849dec7e55c62c
DetedSteel <pkmershin@gmail.com>
2023-11-30 23:53:37 +0300

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".swiper", {
+  speed: 800,
+  spaceBetween: 100,
+  orientation: "horizontal",
+  loop: true,
+  navigation: {
+    nextEl: ".slider__button_next",
+    prevEl: ".slider__button_prev",
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
